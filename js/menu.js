1691472ed7331c81751de1c813674f0d553e7fa0
juniosame <juniosame@hotmail.com>
2023-10-19 16:32:56 -0300

    update JS

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Selecionando o elemento (através de descendência) que acionará o menu
+const botaoMenu = document.querySelector("nav h2");
+console.log(botaoMenu);
+
+//  Selecionando a lista/menu através da classe
+const menu = document.querySelector(".menu");
+console.log(menu);
+
+// Selecionando o link que está dentro do nav h2
+const textoBotao = botaoMenu.querySelector("a")
+console.log(textoBotao);
